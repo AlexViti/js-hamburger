f939b91babe5f460ca1aa28a07d2760a3f6555ca
AlexViti <alexviti12@gmail.com>
2022-02-22 16:02:20 +0100

    Add commented example

--- main.js
+++ main.js
@@ -5,13 +5,10 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 function toggleMenu() {
 	hamburgerMenu.classList.toggle('active');
 }
+
 btnHamburger.addEventListener('click', toggleMenu);
 btnClose.addEventListener('click', toggleMenu);
 
-// btnHamburger.addEventListener('click', function(){
-// 	hamburgerMenu.classList.toggle('active');
-// });
-
-// btnClose.addEventListener('click', function(){
-// 	hamburgerMenu.classList.toggle('active');
-// });
+// function toggleMenu(menu) {
+// 	menu.classList.toggle('active');
+// }
