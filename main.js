54d6b0e25560e972875b1565f9a29e8ca07e49ec
AlexViti <alexviti12@gmail.com>
2022-02-22 15:36:47 +0100

    Improve script

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const btnHamburger = document.querySelector('.header-right>a');
 const btnClose = document.querySelector('.hamburger-menu>a');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 
-
-// btnHamburger.addEventListener('click', openMenu(hamburgerMenu));
-// btnClose.addEventListener('click', closeMenu(hamburgerMenu));
-
-
-btnHamburger.addEventListener('click', function(){
+function toggleMenu() {
 	hamburgerMenu.classList.toggle('active');
-});
+}
+btnHamburger.addEventListener('click', toggleMenu);
+btnClose.addEventListener('click', toggleMenu);
 
-btnClose.addEventListener('click', function(){
-	hamburgerMenu.classList.toggle('active');
-});
+// btnHamburger.addEventListener('click', function(){
+// 	hamburgerMenu.classList.toggle('active');
+// });
+
+// btnClose.addEventListener('click', function(){
+// 	hamburgerMenu.classList.toggle('active');
+// });
